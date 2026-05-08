1df0e32d0eaf4d3bae9c13bbe8dab449295863f0
Ivan Dario Gaitan Sanchez <gaitanivan@gmail.com>
2026-05-08 16:32:59 -0500

    Se actualiza el código para que la ventana inicial inicie maximizada.

--- PORY IA-A/App.xaml.cs
+++ PORY IA-A/App.xaml.cs
@@ -11,7 +11,28 @@ namespace com.gaitanivan.maui.poryiaa
 
 		protected override Window CreateWindow(IActivationState? activationState)
 		{
-			return new Window(new AppShell());
+			var window = new Window(new AppShell());
+			{
+#if WINDOWS
+				// Suscribirse al evento de creación de la ventana nativa
+				window.Created += (s, e) =>
+				{
+					var nativeWindow = window.Handler.PlatformView as Microsoft.UI.Xaml.Window;
+					if (nativeWindow != null)
+					{
+						var windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+						// Usamos Win32Interop para obtener el ID de forma más directa
+						var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+						var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+
+						var presenter = appWindow.Presenter as Microsoft.UI.Windowing.OverlappedPresenter;
+						presenter?.Maximize();
+					}
+				};
+#endif
+			};
+
+			return window;
 		}
 	}
 }
